37d0e7d2d0e48e03b5df5ef916d611c95bbe2836
bogda-n <bogdanpavlyukh@gmail.com>
2022-02-08 10:16:25 +0200

    working version 1.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const fs = require('fs-extra')
 const path = require('path')
-const { default: PQueue } = require('p-queue')
+const {default: PQueue} = require('p-queue')
 const excelToJson = require('simple-excel-to-json')
 const xlsx = require('xlsx')
 
@@ -13,8 +13,9 @@ const baseUrl = 'https://store.canon'
 const fileName = 'canon.xlsx'
 const file = excelToJson.parseXls2Json(path.resolve(__dirname, '_input', fileName))
 
-// const mpn = '1170C003'
-let products = []
+const mpn = '1170C003'
+
+const products = []
 
 const locales = {
   'en': '.co.uk/',
@@ -24,6 +25,7 @@ const locales = {
 }
 
 const createReport = async () => {
+  console.log('start create file report')
   const workBook = xlsx.utils.book_new()
   const workSheet = xlsx.utils.json_to_sheet(products)
 
@@ -37,107 +39,116 @@ const createReport = async () => {
 const processing = async (sku) => {
 
   return new Promise(async (resolve, reject) => {
-  try {
-    let product = {}
-    for (const locale in locales) {
-      let blocks = {}
-      const language = locales[locale]
-      const url = `${baseUrl}${language}${sku.mpn}/`
-      const response = await axios.get(url)
-      const $ = cheerio.load(response.data)
+    try {
+      const product = {}
+      for (const locale in locales) {
+        let blocks = {}
+        const language = locales[locale]
+        const url = `${baseUrl}${language}${sku.mpn}/`
+        // console.log('processing', sku.mpn)
+        try {
+          const response = await axios.get(url)
+          const $ = cheerio.load(response.data)
 
-      $('div.page').each(async (idx, element) => {
-        // if (!$(element).find('[class="error404-bg-container"]')) {
-        //   return
-        // }
-        const head = $(element).find('h1 .pt_bold').text()
-          .replace(/Canon/gmi, '').trim()
-          .replace(/#\S+/gmi, '').trim()
+          $('div.page').each(async (idx, element) => {
 
-        const text = $(element).find('[class="header-5 mom-tab--heading"]').text()
+            const head = $(element).find('h1 .pt_bold').text()
+              .replace(/Canon/gmi, '').trim()
+              .replace(/#\S+/gmi, '').trim()
 
-        const image = 'https:' + $(element)
-          .find('[class="product-image-zoom"]')
-          .find('a').attr('href')
-          .replace(/\?w=\d+&/, '?w=8000&')
+            const text = $(element).find('[class="header-5 mom-tab--heading"]').text()
 
-        product['RequestedBrandName'] = 'Canon'
-        product['RequestedProductCode'] = mpn
-        product['ProductName INT'] = mpn
-        product['Image INT 1'] = image
+            const image = 'https:' + $(element)
+              .find('[class="product-image-zoom"]')
+              .find('a').attr('href')
+              .replace(/\?w=\d+&/, '?w=8000&')
 
-        const translations = {
-          benefits: ['benefits', 'points forts', 'vantaggi', 'vorteile'],
-          package: ['what\'s in the box', 'contenu de la boîte', 'contenuto della confezione', 'lieferumfang'],
-          compatibility: ['compatibility', 'compatibilité', 'compatibilità', 'kompatibilität']
+            product['RequestedBrandName'] = 'Canon'
+            product['RequestedProductCode'] = sku.mpn
+            product['ProductName INT'] = sku.mpn
+            product['Image INT 1'] = image
+
+            const translations = {
+              benefits: ['benefits', 'points forts', 'vantaggi', 'vorteile'],
+              package: ['what\'s in the box', 'contenu de la boîte', 'contenuto della confezione', 'lieferumfang'],
+              compatibility: ['compatibility', 'compatibilité', 'compatibilità', 'kompatibilität']
+            }
+
+            $('.mom-tab--section').each(function () {
+              const name = $(this).children('h4').text().trim()
+
+              if (name) {
+                const value = $(this).children('ul').text().replace(/^\s+/gm, '')
+
+                if (translations.benefits.includes(name.toLowerCase())) {
+                  blocks.benefits = { name, value }
+                }
+
+                if (translations.package.includes(name.toLowerCase())) {
+                  blocks.package = { name, value }
+                }
+
+                if (translations.compatibility.includes(name.toLowerCase())) {
+                  blocks.compatibility = { name, value }
+                }
+              }
+            })
+
+            if (locale === 'en') {
+              product['ProductName EN'] = head
+              product['LongProductName EN'] = head
+              product['MarketingText EN'] = text
+              product['BulletPoints EN'] = blocks.benefits?.value
+              product['URL EN'] = url
+              product['In the box EN'] = blocks.package?.value
+            } else if (locale === 'fr') {
+              product['ProductName FR'] = head
+              product['LongProductName FR'] = head
+              product['MarketingText FR'] = text
+              product['BulletPoints FR'] = blocks.benefits?.value
+              product['URL FR'] = url
+              product['In the box FR'] = blocks.package?.value
+            } else if (locale === 'de') {
+              product['ProductName DE'] = head
+              product['LongProductName DE'] = head
+              product['MarketingText DE'] = text
+              product['BulletPoints DE'] = blocks.benefits?.value
+              product['URL DE'] = url
+              product['In the box DE'] = blocks.package?.value
+            } else if (locale === 'it') {
+              product['ProductName IT'] = head
+              product['LongProductName IT'] = head
+              product['MarketingText IT'] = text
+              product['BulletPoints IT'] = blocks.benefits?.value
+              product['URL IT'] = url
+              product['In the box IT'] = blocks.package?.value
+            }
+
+            product['Compatibility'] = blocks.compatibility?.value
+          })
+        } catch (e) {
+          if (e.response?.status !== 404) {
+            throw new Error(e)
+          }
+          console.error(e.message, '-' , sku.mpn)
+
         }
+      }
 
-        $('.mom-tab--section').each(function () {
-          const name = $(this).children('h4').text().trim()
+      products.push(product)
+      console.log('end processing', sku.mpn)
+      resolve()
 
-          if (!name) {
-            return
-          }
-
-          const value = $(this).children('ul').text().replace(/^\s+/gm, '')
-
-          if (translations.benefits.includes(name.toLowerCase())) {
-            blocks.benefits = { name, value }
-          }
-
-          if (translations.package.includes(name.toLowerCase())) {
-            blocks.package = { name, value }
-          }
-
-          if (translations.compatibility.includes(name.toLowerCase())) {
-            blocks.compatibility = { name, value }
-          }
-        })
-
-        if (locale === 'en') {
-          product['ProductName EN'] = head
-          product['LongProductName EN'] = head
-          product['MarketingText EN'] = text
-          product['BulletPoints EN'] = blocks.benefits.value
-          product['URL EN'] = url
-          product['In the box EN'] = blocks.package.value
-        } else if (locale === 'fr') {
-          product['ProductName FR'] = head
-          product['LongProductName FR'] = head
-          product['MarketingText FR'] = text
-          product['BulletPoints FR'] = blocks.benefits.value
-          product['URL FR'] = url
-          product['In the box FR'] = blocks.package.value
-        } else if (locale === 'de') {
-          product['ProductName DE'] = head
-          product['LongProductName DE'] = head
-          product['MarketingText DE'] = text
-          product['BulletPoints DE'] = blocks.benefits.value
-          product['URL DE'] = url
-          product['In the box DE'] = blocks.package.value
-        } else if (locale === 'it') {
-          product['ProductName IT'] = head
-          product['LongProductName IT'] = head
-          product['MarketingText IT'] = text
-          product['BulletPoints IT'] = blocks.benefits.value
-          product['URL IT'] = url
-          product['In the box IT'] = blocks.package.value
-        }
-
-        product['Compatibility'] = blocks.compatibility.value
-      })
+    } catch (e) {
+      console.error(e.message)
+      resolve()
     }
-    products.push(product)
-    resolve()
-  } catch (e) {
-    console.error(e)
-  }
   })
 }
 
 const start = async () => {
   try {
-    const queue = new PQueue({ concurrency: 30 })
+    const queue = new PQueue({ concurrency: 5 })
     queue.on('add', () => {
       console.log(`Task is added.  Size: ${queue.size}  Pending: ${queue.pending}`)
     })
@@ -148,11 +159,11 @@ const start = async () => {
       console.log('queue is clean', new Date())
       createReport()
     })
+    // queue.add(() => Promise.resolve())
     for (let sku of file[0]) {
       queue.add(() => processing(sku))
+    //   await processing(sku)
     }
-
-
   } catch (error) {
     console.error(error)
   }
